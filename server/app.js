75246eb83fb05d8d304321d34a6178c5d4d1e9aa
Xavi <valcreativo@gmail.com>
2024-05-20 16:33:43 +0200

    day 1 complete

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors")
 const PORT = 5005;
+const cohortData = require(__dirname + "/cohorts.json") //se puede usar sin dirname.
+const studentsData = require(__dirname + "/students.json")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -15,6 +18,12 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+//app.use(cors()) //deja entrar a cualquiera
+
+app.use(cors({
+  origin: ['http://localhost:5173']
+}))
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -28,7 +37,13 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+app.get("/api/cohorts", (req, res)=>{
+  res.json(cohortData)
 
+})
+app.get("/api/students", (req, res)=>{
+  res.json(studentsData)
+})
 
 // START SERVER
 app.listen(PORT, () => {
