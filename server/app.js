162950230f70723d90d3933dc0bf275220bbe747
Asier MD <asier.md09@gmail.com>
2024-05-22 14:37:35 +0200

    mover listen abajo del todo y comentar cosas

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ const StudentSchema = require("./models/Students.model.js")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
@@ -22,16 +21,13 @@ mongoose
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+
 app.use(cors({
   origin: ['http://localhost:5173']
 })) //deja entrar lo que especifiquemos
-//app.use(cors()) //deja entrar a cualquiera
-
-
+//app.use(cors({origin:'*'})) //deja entrar a cualquiera
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -42,7 +38,8 @@ app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
+
+//* RUTAS CONECTADAS CON JSON
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
@@ -53,12 +50,7 @@ app.get("/api/students", (req, res)=>{
   res.json(studentsData)
 })
 
-// START SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-
+//* RUTAS CONECTADAS CON MONGOOSE/MONGODB
 app.get("/cohorts", (req, res) => {
   CohortSchema.find({})
     .then((cohorts) => {
@@ -80,4 +72,11 @@ app.get("/students", (req, res) => {
       console.error("Error while retrieving books ->", error);
       res.status(500).json({ error: "Failed to retrieve books" });
     });
+});
+
+
+
+//* START SERVER
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
